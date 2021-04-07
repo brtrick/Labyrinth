a11c40f0911ac5202b1ca952aa766bd3c338dd4e
Brad Trick <btrick@gmail.com>
2021-04-06 22:22:23 -0400

    Add textures to game pieces

--- src/gameLogic/piece.js
+++ src/gameLogic/piece.js
@@ -1,5 +1,14 @@
 import * as THREE from "three";
 
+const textureLoader = new THREE.TextureLoader ();
+// const texture = textureLoader.load("/src/assets/textures/bamboo-wood-semigloss-albedo.png");
+const texture = textureLoader.load("/src/assets/textures/AnyConv.com__TexturesCom_Wood_ZebranoVeneer_512_albedo.png");
+const roughness = textureLoader.load("src/assets/textures/AnyConv.com__TexturesCom_Wood_ZebranoVeneer_512_roughness.png");
+const normal = textureLoader.load("src/assets/textures/AnyConv.com__TexturesCom_Wood_ZebranoVeneer_512_normal.png")
+const textureD = textureLoader.load("/src/assets/textures/AnyConv.com__TexturesCom_Wood_Wenge_512_albedo.png");
+const roughnessD = textureLoader.load("src/assets/textures/AnyConv.com__TexturesCom_Wood_Wenge_512_roughness.png");
+const normalD = textureLoader.load("src/assets/textures/AnyConv.com__TexturesCom_Wood_Wenge_512_normal.png")
+
 export default class Piece {
     constructor( dark, box, solid, tall) {
         this.tall = tall;
@@ -14,7 +23,9 @@ export default class Piece {
         const geometry = this.box ?
             new THREE.BoxGeometry(1, (this.tall ? 3 : 1.5), 1) :
             new THREE.CylinderGeometry(.5, .5, (this.tall ? 3 : 1.5), 100);
-        const material = new THREE.MeshPhongMaterial( { color: (this.dark ? 0x483C32 : 0xfbf3d4 )} );
+        const material = this.dark ?
+            new THREE.MeshStandardMaterial( { map: textureD, normalMap: normalD, roughnessMap: roughnessD} ) :
+            new THREE.MeshStandardMaterial( { map: texture, normalMap: normal, roughnessMap: roughness} );
         return new THREE.Mesh( geometry, material );
     }
 }
--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import piecePool from "./gameLogic/piecePool"
 
 const p = new piecePool();
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-// const camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 0, 100 );
+// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 0, 100 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -15,24 +15,14 @@ document.body.appendChild( renderer.domElement );
 
 // const controls = new OrbitControls(camera, renderer.domElement);
 const controls = new TrackballControls(camera, renderer.domElement);
-// const geometry1 = new THREE.BoxGeometry(1, 3, 1);
-// const material = new THREE.MeshPhongMaterial( { color: 0xfbf3d4 } );
-// const column = new THREE.Mesh( geometry1, material );
-// scene.add( column );
-
-// const geometry2 = new THREE.CylinderGeometry(.5, .5, 3, 100);
-// const material2 = new THREE.MeshPhongMaterial( { color: 0x483c32})
-// const cylinder = new THREE.Mesh( geometry2, material2);
-// cylinder.position.set (5, 0, 0);
-// scene.add( cylinder );
 
 for (let i=0; i<16; i++) {
     p.pool[i].model.position.set ((i%4)*3, (i/4)*5, 0);
     scene.add (p.pool[i].model);
 }
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-const dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.8);
 dirLight.position.set(10,20,5);
 scene.add(ambientLight);
 scene.add(dirLight);
