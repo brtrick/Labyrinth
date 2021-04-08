7c19377f1515a45cfb6e9995b42dc0dc8c6b4b9a
Brad Trick <btrick@gmail.com>
2021-04-08 12:59:39 -0400

    Moved assets to dist folder

--- src/gameLogic/piece.js
+++ src/gameLogic/piece.js
@@ -3,12 +3,12 @@ import { BufferGeometryUtils } from "three/examples/jsm/utils/BufferGeometryUtil
 
 const textureLoader = new THREE.TextureLoader ();
 // const texture = textureLoader.load("/src/assets/textures/bamboo-wood-semigloss-albedo.png");
-const texture = textureLoader.load("/src/assets/textures/AnyConv.com__TexturesCom_Wood_ZebranoVeneer_512_albedo.png");
-const roughness = textureLoader.load("src/assets/textures/AnyConv.com__TexturesCom_Wood_ZebranoVeneer_512_roughness.png");
-const normal = textureLoader.load("src/assets/textures/AnyConv.com__TexturesCom_Wood_ZebranoVeneer_512_normal.png")
-const textureD = textureLoader.load("/src/assets/textures/AnyConv.com__TexturesCom_Wood_Wenge_512_albedo.png");
-const roughnessD = textureLoader.load("src/assets/textures/AnyConv.com__TexturesCom_Wood_Wenge_512_roughness.png");
-const normalD = textureLoader.load("src/assets/textures/AnyConv.com__TexturesCom_Wood_Wenge_512_normal.png")
+const texture = textureLoader.load("dist/assets/textures/ZebranoVeneer_512_albedo.png");
+const roughness = textureLoader.load("dist/assets/textures/ZebranoVeneer_512_roughness.png");
+const normal = textureLoader.load("dist/assets/textures/ZebranoVeneer_512_normal.png")
+const textureD = textureLoader.load("dist/assets/textures/Wenge_512_albedo.png");
+const roughnessD = textureLoader.load("dist/assets/textures/Wenge_512_roughness.png");
+const normalD = textureLoader.load("dist/assets/textures/Wenge_512_normal.png")
 
 export default class Piece {
     constructor( dark, box, hollow, tall) {
